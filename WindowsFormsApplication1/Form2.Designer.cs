5db7e0bd0fe3edce03677bfaca3a028f17832798
sergdarkjob@gmail.com <sergdarkjob@gmail.com>
2018-09-25 12:25:34 +0300

    20180925

--- WindowsFormsApplication1/Form2.Designer.cs
+++ WindowsFormsApplication1/Form2.Designer.cs
@@ -45,24 +45,27 @@
             // label3
             // 
             this.label3.AutoSize = true;
-            this.label3.Location = new System.Drawing.Point(24, 17);
+            this.label3.Location = new System.Drawing.Point(5, 3);
             this.label3.Name = "label3";
             this.label3.Size = new System.Drawing.Size(87, 13);
             this.label3.TabIndex = 4;
             this.label3.Text = "Подразделение";
+            this.label3.Visible = false;
             // 
             // label4
             // 
             this.label4.AutoSize = true;
-            this.label4.Location = new System.Drawing.Point(24, 59);
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(5, 28);
             this.label4.Name = "label4";
-            this.label4.Size = new System.Drawing.Size(64, 13);
+            this.label4.Size = new System.Drawing.Size(96, 20);
             this.label4.TabIndex = 5;
             this.label4.Text = "Имя файла";
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(192, 97);
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(173, 66);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(147, 40);
             this.button1.TabIndex = 6;
@@ -72,7 +75,8 @@
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(345, 97);
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button2.Location = new System.Drawing.Point(326, 66);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(144, 40);
             this.button2.TabIndex = 7;
@@ -82,44 +86,48 @@
             // 
             // textBox1
             // 
-            this.textBox1.Location = new System.Drawing.Point(127, 14);
+            this.textBox1.Location = new System.Drawing.Point(108, 0);
             this.textBox1.Name = "textBox1";
             this.textBox1.Size = new System.Drawing.Size(303, 20);
             this.textBox1.TabIndex = 8;
+            this.textBox1.Visible = false;
             // 
             // textBox2
             // 
-            this.textBox2.Location = new System.Drawing.Point(127, 56);
+            this.textBox2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.textBox2.Location = new System.Drawing.Point(108, 25);
             this.textBox2.Name = "textBox2";
-            this.textBox2.Size = new System.Drawing.Size(303, 20);
+            this.textBox2.Size = new System.Drawing.Size(303, 26);
             this.textBox2.TabIndex = 9;
             // 
             // label5
             // 
             this.label5.AutoSize = true;
             this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label5.Location = new System.Drawing.Point(446, 14);
+            this.label5.Location = new System.Drawing.Point(427, 0);
             this.label5.Name = "label5";
             this.label5.Size = new System.Drawing.Size(24, 20);
             this.label5.TabIndex = 10;
             this.label5.Text = "...";
+            this.label5.Visible = false;
             // 
             // label6
             // 
             this.label6.AutoSize = true;
             this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label6.Location = new System.Drawing.Point(469, 16);
+            this.label6.Location = new System.Drawing.Point(450, 2);
             this.label6.Name = "label6";
             this.label6.Size = new System.Drawing.Size(20, 20);
             this.label6.TabIndex = 11;
             this.label6.Text = "X";
+            this.label6.Visible = false;
             this.label6.Click += new System.EventHandler(this.label6_Click);
             // 
             // label7
             // 
             this.label7.AutoSize = true;
             this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label7.Location = new System.Drawing.Point(469, 54);
+            this.label7.Location = new System.Drawing.Point(450, 23);
             this.label7.Name = "label7";
             this.label7.Size = new System.Drawing.Size(20, 20);
             this.label7.TabIndex = 13;
@@ -129,10 +137,10 @@
             // label8
             // 
             this.label8.AutoSize = true;
-            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
-            this.label8.Location = new System.Drawing.Point(446, 52);
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label8.Location = new System.Drawing.Point(427, 21);
             this.label8.Name = "label8";
-            this.label8.Size = new System.Drawing.Size(24, 20);
+            this.label8.Size = new System.Drawing.Size(21, 20);
             this.label8.TabIndex = 12;
             this.label8.Text = "...";
             this.label8.Click += new System.EventHandler(this.label8_Click);
@@ -153,14 +161,14 @@
             this.panel1.Dock = System.Windows.Forms.DockStyle.Fill;
             this.panel1.Location = new System.Drawing.Point(0, 0);
             this.panel1.Name = "panel1";
-            this.panel1.Size = new System.Drawing.Size(531, 157);
+            this.panel1.Size = new System.Drawing.Size(480, 120);
             this.panel1.TabIndex = 14;
             // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(531, 157);
+            this.ClientSize = new System.Drawing.Size(480, 120);
             this.Controls.Add(this.panel1);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedToolWindow;
             this.Name = "Form2";
--- WindowsFormsApplication1/Form2.cs
+++ WindowsFormsApplication1/Form2.cs
@@ -1,16 +1,9 @@
 ﻿using FirebirdSql.Data.FirebirdClient;
 using System;
-using System.Collections.Generic;
-using System.ComponentModel;
 using System.Data;
-using System.Data.Entity.Infrastructure;
-using System.Drawing;
 using System.IO;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using WindowsFormsApplication1;
 
 namespace WindowsFormsApplication1
 {
@@ -18,6 +11,7 @@ namespace WindowsFormsApplication1
     {
         FbConnection fb; //fb ссылается на соединение с нашей базой данных, по-этому она должна быть доступна всем методам нашего класса
         public string path_db;
+        bool status = false;
 
         public Form2()
         {
@@ -32,9 +26,10 @@ namespace WindowsFormsApplication1
             if (OPF.ShowDialog() == DialogResult.OK)
             {
                 textBox2.Text = OPF.FileName;
-
-                //MessageBox.Show(OPF.FileName);
+                status = true;
             }
+            else
+                status = false;
         }
 
         private void button2_Click(object sender, EventArgs e)
@@ -53,6 +48,7 @@ namespace WindowsFormsApplication1
             fb_con.ServerType = 0; //указываем тип сервера (0 - "полноценный Firebird" (classic или super server), 1 - встроенный (embedded))
             fb = new FbConnection(fb_con.ToString()); //передаем нашу строку подключения объекту класса FbConnection
 
+            #region example how fill
             // RepModel r = new RepModel();
             //MessageBox.Show(r.Select(fb).Name);
 
@@ -88,65 +84,55 @@ namespace WindowsFormsApplication1
             //        }
             //    }
             //}
-
+            #endregion
 
             try
             {
                 PriceTabel(fb, null, null, null, null);
 
                 if (!string.IsNullOrEmpty(textBox2.Text))
-                {
-                    using (var w = new StreamWriter(textBox2.Text))
-                    {
-                        foreach (DataColumn column in PriceTabel(fb, null, null, null, null).Columns)
-                        {
-                            w.Write($"{column.ColumnName};");
-                        }
-                        w.Write("\n");
-
-                        foreach (DataRow dataRow in PriceTabel(fb, null, null, null, null).AsEnumerable().ToList())
-                        {
-                            var first = dataRow[0].ToString(); // 
-                            var second = dataRow[1].ToString(); //
-                            var third = dataRow[2].ToString(); //
-                            var fourth = dataRow[3].ToString(); //
-                            var line = string.Format($"{first};{second};{third};{fourth}");
-                            w.WriteLine(line);
-                            w.Flush();
-                        }
-                    }
-                }
+                    Fillcsv();
+                
                 else
                 {
                     label8_Click(sender, e);
                     if (!string.IsNullOrEmpty(textBox2.Text))
-                    {
-                        using (var w = new StreamWriter(textBox2.Text))
-                        {
-                            foreach (DataColumn column in PriceTabel(fb, null, null, null, null).Columns)
-                            {
-                                w.Write($"{column.ColumnName};");
-                            }
-                            w.Write("\n");
-
-                            foreach (DataRow dataRow in PriceTabel(fb, null, null, null, null).AsEnumerable().ToList())
-                            {
-                                var first = dataRow[0].ToString(); // 
-                                var second = dataRow[1].ToString(); //
-                                var third = dataRow[2].ToString(); //
-                                var fourth = dataRow[3].ToString(); //
-                                var line = string.Format($"{first};{second};{third};{fourth}");
-                                w.WriteLine(line);
-                                w.Flush();
-                            }
-                        }
-                    }
+                        Fillcsv();
                 }
-                MessageBox.Show("Выгрузка успешно");
+                if (status)
+                    MessageBox.Show("выгружено");
             }
             catch (Exception ex)
             {
                 MessageBox.Show($"Выгрузка неудачно {ex.Message}");
+            }
+        }
+
+        private void Fillcsv()
+        {
+            using (var w = new StreamWriter(textBox2.Text))
+            {
+                int ColumnsCount = PriceTabel(fb, null, null, null, null).Columns.Count;
+                string line = "";
+
+                foreach (DataColumn column in PriceTabel(fb, null, null, null, null).Columns)
+                {
+                    w.Write($"{column.ColumnName};");
+                }
+                w.Write("\n");
+
+                foreach (DataRow dataRow in PriceTabel(fb, null, null, null, null).AsEnumerable().ToList())
+                {
+                    line = "";
+                    for (int i = 0; i < ColumnsCount; i++)
+                    {
+                        string first = dataRow[i].ToString().Replace(@",", ".") + ";";  
+                        line = line + first;
+                    }
+                     w.WriteLine(line);
+                    
+                    w.Flush();
+                }
             }
         }
 
@@ -209,9 +195,10 @@ namespace WindowsFormsApplication1
             if (string.IsNullOrEmpty(REFRESH_ID)) { REFRESH_ID = "null"; }
             if (string.IsNullOrEmpty(IN_ORG_ID)) { IN_ORG_ID = "null"; }
 
-            string query = $"SELECT ID, CODE, NAME, UNIT FROM Z$DIC_PRICE_S({GRP_ID}, {FILTER_}, {REFRESH_ID}, {IN_ORG_ID})";
+            //string query = $"SELECT ID, CODE, NAME, UNIT FROM Z$DIC_PRICE_S({GRP_ID}, {FILTER_}, {REFRESH_ID}, {IN_ORG_ID})";
+            string query = $"select GOOD_ID, ORG_ID, TYPE_BONUS_ID, TYPE_PRICE_ID, PRICE_OUT_DISC from DIC_PRICE_LIST order by GOOD_ID";
 
-            // MessageBox.Show($"{query}");
+              // MessageBox.Show($"{query}");
             FbCommand cmd = new FbCommand(query, conn);
 
             try
